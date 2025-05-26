540836009d66ce67024c170abd3f626f2b66acaa
KaMiyatake <come_on_myself@hotmail.com>
2025-05-26 23:41:13 +0900

    feat　ヘッダー部の不要なリンクを削除

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -13,23 +13,23 @@ const Header: React.FC<HeaderProps> = ({ categories }) => {
       <div className={styles.container}>
         <div className={styles.logo}>
           <Link href="/" passHref>
-            <h1>Game News Portal テスト中</h1>
+            <h1>Game News Portal テスト</h1>
           </Link>
         </div>
         <nav className={styles.nav}>
           <ul className={styles.navList}>
-            <li className={styles.navItem}>
+            {/* <li className={styles.navItem}>
               <Link href="/" passHref>
                 <span className={styles.navLink}>ホーム</span>
               </Link>
-            </li>
-            {categories.map((category) => (
+            </li> */}
+            {/* {categories.map((category) => (
               <li key={category.id} className={styles.navItem}>
                 <Link href={`/category/${category.slug}`} passHref>
                   <span className={styles.navLink}>{category.name}</span>
                 </Link>
               </li>
-            ))}
+            ))} */}
             <li className={styles.navItem}>
               <Link href="/about" passHref>
                 <span className={styles.navLink}>サイトについて</span>
